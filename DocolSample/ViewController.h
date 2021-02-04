d55059f45f46725eed081958c3a1181d0d57a063
XUTAO HUANG <xutaohuang@XUTAOdeMacBook-Pro.local>
2021-02-04 15:53:33 +0800

    init commit

---
+++ DocolSample/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  DocolSample
+//
+//  Created by XUTAO HUANG on 2021/2/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DocolSample/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  DocolSample
+//
+//  Created by XUTAO HUANG on 2021/2/4.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
